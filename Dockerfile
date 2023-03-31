24a37149971dc2321b1df26438affb8eb54a225a
osa ryusei <ryusei-osa@J0007.geniee.jp sg2.geniee.jp ty5.geniee.jp vm.geniee.jp>
2023-03-31 12:12:17 +0900

    Fix: ３回のスクショを同時に行うように修正

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-FROM public.ecr.aws/lambda/nodejs:14
-  
+FROM public.ecr.aws/lambda/nodejs:14-x86_64
+
 COPY index.js package.json package-lock.json .env /var/task/ 
 RUN npm install
 
--- index.js
+++ index.js
@@ -18,14 +18,73 @@ const num_images = Number(process.env.NUM_IMAGES);
 
 const pageOptions = {
   extraHTTPHeaders: { "Accept-Language": "ja" },
-  userAgent:
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
   viewport: {
     width: 640,
     height: 360,
     deviceScaleFactor: 1,
   },
 };
+async function screenshot(target_html, dir, num) {
+  const server = await http.createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    response.write(target_html);
+    response.end();
+  });
+
+  await server.listen("3000", "0.0.0.0");
+
+  const browser = await playwright.launchChromium({
+    headless: false,
+  });
+  const page = await browser.newPage(pageOptions);
+  const client = await page.context().newCDPSession(page);
+  await client.send("Network.emulateNetworkConditions", {
+    offline: false,
+    downloadThroughput: (15360 * 1000) / 8, // 15360kbps
+    uploadThroughput: (15360 * 1000) / 8, // 15360kbps
+    latency: 100,
+  });
+
+  page.goto("http://localhost:3000", { timeout: 0 });
+
+  const img_url = [];
+  const keys = [];
+  const shots = [];
+  const times = [];
+  const start = Date.now();
+
+  for (let i = 0; i < num_images; i++) {
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    page.screenshot().then(async (shot) => {
+      const time = Date.now() - start;
+      keys.push(dir + `/${num}_${String(time).padStart(5, "0")}.jpg`);
+      shots.push(shot);
+      times.push(time);
+    });
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  await browser.close();
+  server.close();
+  for (let i = 0; i < shots.length; i++) {
+    await S3.putObject({
+      Bucket: bucket,
+      ContentType: "image/jpeg",
+      Key: keys[i],
+      Body: shots[i],
+    }).promise();
+
+    img_url.push({
+      image: S3.getSignedUrl("getObject", {
+        Bucket: bucket,
+        Key: keys[i],
+        Expires: 86400,
+      }),
+      time: times[i],
+    });
+  }
+  return img_url;
+}
 
 exports.handler = async (event) => {
   await playwright.loadFont(
@@ -35,86 +94,37 @@ exports.handler = async (event) => {
   let html = null;
   let dir = null;
   let base_url = null;
-
+  if (event.html && event.dir && event.base_url) {
+    html = await Buffer.from(event.html, "base64");
+    dir = event.dir;
+    base_url = event.base_url;
+  }
   if (event.body) {
     const body = JSON.parse(event.body);
-    html = Buffer.from(body.html, "base64");
+    html = await Buffer.from(body.html, "base64");
     dir = body.dir;
     base_url = body.base_url;
   }
+  if (!html || !dir || !base_url) return { message: "invalid argment" };
 
-  if (dir && html) {
-    let browser = await playwright.launchChromium();
-    let page = await browser.newPage(pageOptions);
-    const $ = cheerio.load(html);
-    $("*").each(function () {
-      let src = $(this).attr("src");
-      let href = $(this).attr("href");
-      if (src) {
-        src = new url.URL(src, base_url);
-        $(this).attr("src", src);
-      }
-      if (href) {
-        href = new url.URL(href, base_url);
-        $(this).attr("href", href);
-      }
-    });
-
-    const server = await http.createServer((request, response) => {
-      response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-      response.write($.html());
-      response.end();
-    });
-
-    let images = [];
-    await server.listen("3000", "0.0.0.0");
-
-    browser = await playwright.launchChromium();
-    page = await browser.newPage(pageOptions);
-    const client = await page.context().newCDPSession(page);
-    await client.send("Network.emulateNetworkConditions", {
-      offline: false,
-      downloadThroughput: (15360 * 1000) / 8, // 15360kbps
-      uploadThroughput: (15360 * 1000) / 8, // 15360kbps
-      latency: 100,
-    });
-    page
-      .goto("http://localhost:3000", { timeout: 0 })
-      .then(() => {})
-      .catch(() => {});
-
-    let paths = [];
-    let shots = [];
-    for (let i = 0; i < num_images; i++) {
-      await page.waitForTimeout(100);
-      shots.push(await page.screenshot());
-      paths.push(dir + `/screenshot_${String(i + 1).padStart(5, "0")}.jpg`);
+  const $ = await cheerio.load(html);
+  $("*").each(function () {
+    let src = $(this).attr("src");
+    let href = $(this).attr("href");
+    if (src) {
+      src = new url.URL(src, base_url);
+      $(this).attr("src", src);
     }
-    for (let i = 0; i < num_images; i++) {
-      await S3.putObject({
-        Bucket: bucket,
-        ContentType: "image/jpeg",
-        Key: paths[i],
-        Body: shots[i],
-      }).promise();
-
-      images.push(
-        S3.getSignedUrl("getObject", {
-          Bucket: bucket,
-          Key: paths[i],
-          Expires: 86400,
-        })
-      );
+    if (href) {
+      href = new url.URL(href, base_url);
+      $(this).attr("href", href);
     }
+  });
+  const data = [];
 
-    await browser.close();
-    server.close();
-
-    return {
-      message: "Screenshot succeed",
-      images: images,
-    };
-  } else {
-    return { message: "Screenshot failed" };
+  for (let i = 0; i < 3; i++) {
+    data.push(await screenshot($.html(), dir, i));
   }
+
+  return data;
 };
